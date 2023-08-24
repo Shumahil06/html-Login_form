fe8e261b3181044b9922789dddb6a0a1c297d8a0
Shumahil06 <140594839+Shumahil06@users.noreply.github.com>
2023-08-24 17:53:31 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const signUpButton= document.getElementById('signsUp'); 
+const signInButton =document.getElementy('signin');
+const container= document.getElementByID('container');
+
+signUpButton.addEventListener('click',()=>
+{   containerlist.classList.add("right-panel-active");  });
+
+signInButton.addEventListener('click',()=>
+{   containerlist.classList.add("right-panel-active");  });
+
+
